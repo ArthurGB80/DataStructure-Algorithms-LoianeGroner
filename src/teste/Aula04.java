e68bbd885ac10a5aac5b2434774a012063cf73a6
arthurgb80 <arthurgularte80@gmail.com>
2023-10-17 16:58:03 +0300

    Aula 4

---
+++ src/teste/Aula04.java
@@ -0,0 +1,21 @@
+package teste;
+
+import vetor.Vetor;
+
+public class Aula04 {
+
+    public static void main(String[] args) throws Exception {
+
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+
+        System.out.println(vetor.tamanho());
+
+        System.out.println(vetor);
+
+    }
+}
+
