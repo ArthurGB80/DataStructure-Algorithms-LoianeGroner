e229496d23b4fc438bdad30694f8ca9e21ec3f97
arthurgb80 <arthurgularte80@gmail.com>
2023-10-17 17:37:25 +0300

    Aula06

---
+++ src/teste/Aula06.java
@@ -0,0 +1,19 @@
+package teste;
+
+import vetor.Vetor;
+
+public class Aula06 {
+
+    public static void main(String[] args) throws Exception {
+
+
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+
+        System.out.println(vetor.busca("Elemento 1"));
+        System.out.println(vetor.busca("elemento 2"));
+    }
+}
